76614a9c06d4268dd72a0aec771e600ddcda5533
Tim Biles <timbilestimbiles@gmail.com>
2018-12-18 16:31:51 -0600

    menu solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      links: ['Home', 'About', 'Services', 'Contact'],
+      menu: 'close'
+    };
+  }
+
+  menu = () => {
+    const { menu } = this.state;
+
+    if (menu === 'open') {
+      this.setState({ menu: 'close' })
+    } else {
+      this.setState({ menu: 'open' });
+    }
+  };
   render() {
+    const map = this.state.links.map((e, i) => {
+      return <li key={i}>{e}</li>;
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <section className="nav_bar">
+            <div>
+              <p>Start Bootstrap</p>
+            </div>
+            <ul className="menu">{map}</ul>
+            <span className="burger" onClick={() => this.menu()} />
+          </section>
+        </nav>
+        <section id={this.state.menu} className="hidden_menu">
+          {this.state.menu === 'open' && <ul>{map}</ul>}
+        </section>
       </div>
     );
   }
